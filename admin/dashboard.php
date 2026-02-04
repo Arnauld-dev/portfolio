a276473e8b935b8a3238e879eace6c87b0bad054
Arnauld-dv <irankundaaimearnauld@gmail.com>
2026-02-04 08:42:38 +0200

    style dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -8,16 +8,22 @@
     <title>Document</title>
 </head>
 <body>
-    <div>
+    <div class="container">
+        <div>
         <?php include __DIR__ .'/includes/sidebar.php'; ?>
+        </div>
+        <div>
+            <div>
+            <h1>Admin Dashboard</h1>
+            
+        </div>
+        <div>
+            <a href="settings.php">Projet</a>
+            <a href="users.php">User Management</a>
+            <a href="Messages.php">Message</a>
+        </div>
+        </div>
     </div>
-    <h1>Admin Dashboard</h1>
-    <p>Welcome to the admin dashboard. Here you can manage the application settings and user accounts.</p>
-    <div>
-        <a href="settings.php">Projet</a>
-        <a href="users.php">User Management</a>
-        <a href="Messages.php">Message</a>
-
-    </div>
+    
 </body>
 </html>
--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -5,10 +5,10 @@
     <ul>
         <li><a href="dashboard.php">Dashboard</a></li>
         <li><a href="users.php">Users</a></li>
-        <li><a href="settings.php">Settings</a></li>
+        <li ><a href="project.php">Project</a></li>
         <li><a href="messages.php">Messages</a></li>
         <li><a href="service.php">Services</a></li>
-        <li ><a href="project.php">Project</a></li>
+        <li><a href="settings.php">Settings</a></li>
         <li><a href="logout.php">Logout</a></li>
     </ul>
 </div>
