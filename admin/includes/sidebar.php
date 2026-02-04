e33c29acca640c96c66dd739626193c29e6df1f7
Arnauld-dv <irankundaaimearnauld@gmail.com>
2026-02-04 07:08:10 +0200

    sidebar

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sidebar.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="sidebar-header">
+            <h1>Admin Panel Controler</h1>
+        </div>
+        <ul>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="users.php">Users</a></li>
+            <li><a href="settings.php">Settings</a></li>
+            <li><a href="messages.php">Messages</a></li>
+            <li><a href="service.php">Services</a></li>
+            <li ><a href="project.php">Project</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </div>
+</body>
+</html>
